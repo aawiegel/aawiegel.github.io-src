2c4c887434b8746fa2854f6042a295daabf601f7
Aaron Wiegel <aawiegel@gmail.com>
2018-10-25 01:28:39 -0700

    fixed theme, url

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'US/Pacific'
 
 DEFAULT_LANG = 'en'
 
+THEME = '/home/aaron/projects/pelican-themes/blueidea'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
